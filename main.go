56d4e94b4749ef136813e102a01c91d330e4f941
asdwdev <asdwdev@gmail.com>
2025-12-04 23:35:08 +0700

    fungsi sederhana

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
-// latihan bagian 4
+// Fungsi = blok kode yang bisa dipanggil berulang-ulang.
 package main
 
 import "fmt"
 
+func hello() {
+	fmt.Println("halo dari fungsi!")
+}
+
 func main() {
-	var x int
-	fmt.Print("masukkan angka: ")
-	fmt.Scanln(&x)
-
-	if x%2 == 0 {
-		fmt.Println("angka genap")
-	} else {
-		fmt.Println("angka ganjil")
-	}
-
-	i := 1
-	for i <= x {
-		fmt.Println(i)
-		i++
-	}
-
-	switch {
-	case x > 10:
-		fmt.Println("angka besar")
-	}
+	hello()
 }
