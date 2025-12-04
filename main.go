aa27e8e8fef5d4a44c3e9beb00d4198c9425df54
asdwdev <asdwdev@gmail.com>
2025-12-05 00:00:17 +0700

    latihan

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
-// 6️⃣ Fungsi tidak boleh dideklarasi di dalam fungsi lain
+// latihan bagian 5
 package main
 
-// harus di luar fungsi
+import "fmt"
 
-func test()
+func kelilingPersegi(sisi int) int {
+	return 4 * sisi
+}
+
+func luasPersegi(sisi int) int {
+	return sisi * sisi
+}
+
+func hitung(sisi int) (int, int) {
+	return kelilingPersegi(sisi), luasPersegi(sisi)
+}
 
 func main() {
-	// func test() //error
+	var sisi int
+	fmt.Print("masukkan sisi: ")
+	fmt.Scanln(&sisi)
+
+	keliling, luas := hitung(sisi)
+	fmt.Println("keliling:", keliling)
+	fmt.Println("luas:", luas)
 }
