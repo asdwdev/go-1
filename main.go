a179583137f4c5d58193f019bffb3a6e674124be
asdwdev <asdwdev@gmail.com>
2025-12-05 00:16:26 +0700

    latihan

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
-// 4️⃣ MAP (key-value)
-
+// latihan bagian 6
 package main
 
 import "fmt"
 
 func main() {
-	// Contoh map string → int:
+	// === angka ===
+	angka := []int{1, 2, 3, 4, 5}
+
+	angka = append(angka, 6, 7)
+
+	for _, v := range angka {
+		fmt.Println(v)
+	}
+
+	// === buah ===
 	m := map[string]int{
 		"apel":   3,
-		"pisang": 5,
+		"mangga": 5,
+		"jeruk":  2,
 	}
-	fmt.Println(m)
 
-	// delete element map
-	delete(m, "apel")
-	fmt.Println(m)
+	m["pisang"] = 10
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 
 }
