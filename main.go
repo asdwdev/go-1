70194e88feb1c89339d0618c1932e4139245b9d3
asdwdev <asdwdev@gmail.com>
2025-12-08 21:06:06 +0700

    latihan

--- main.go
+++ main.go
@@ -1,45 +1,29 @@
-// custom error + wrapping + errors.Is
+// latihan
 package main
 
 import (
 	"errors"
 	"fmt"
+	"math"
 )
 
-type SalahInputError struct {
-	Pesan string
-}
-
-func (e SalahInputError) Error() string {
-	return e.Pesan
-}
-
-// 2. Fungsi yang mengembalikan error terbungkus (fmt.Errorf("%w"))
-func cekUmur(umur int) error {
-	if umur < 18 {
-		err := SalahInputError{Pesan: "umur harus >= 18"}
-		return fmt.Errorf("validasi gagal: %w", err) // wrap error
+func akar(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("tidak bisa akar bilangan negatif")
 	}
-	return nil
+
+	return math.Sqrt(x), nil
 }
 
-// 3. Cara mengecek apakah error tersebut adalah SalahInputError
-// Gunakan errors.As (lebih cocok daripada Is untuk tipe struct).
 func main() {
-	err := cekUmur(15)
+	var angka float64
+	fmt.Print("Masukkan sebuah angka: ")
+	fmt.Scanln(&angka)
+
+	v, err := akar(angka)
 	if err != nil {
-		var inputErr SalahInputError
-
-		// cek apakah err mengandung SalahInputError (meskipun di wrap)
-		if errors.As(err, &inputErr) {
-			fmt.Println("terjadi error input:", inputErr.Pesan)
-			return
-		}
-
-		// fallback error biasa
-		fmt.Println("error lain:", err)
+		fmt.Println("error:", err)
+		return
 	}
+	fmt.Println("hasil:", v)
 }
-
-// errors.Is() cocok untuk sentinel error (var errSomething = errors.New("…"))
-// Tapi untuk custom struct error, yang benar pakai errors.As().
