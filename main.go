0128b52ae972ff3b6d6a2477e3e672691cc07430
asdwdev <asdwdev@gmail.com>
2025-12-08 20:43:21 +0700

    custom erro dengan data tambahan (tetap simpel)

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
-// ✅ 2. Custom error tapi minimalis (pakai struct, tapi ringkas)
+// Custom error dengan data tambahan (tetap simpel)
 package main
 
-type SalahInputError string
+import "fmt"
 
-func (e SalahInputError) Error() string {
-	return string(e)
+type SalahInputError struct {
+	Field string
+}
+
+func (e *SalahInputError) Error() string {
+	return "input salah pada field:" + e.Field
 }
 
 func main() {
-	err := SalahInputError("input salah banget")
-	println(err.Error())
+	err := &SalahInputError{Field: "username"}
+	fmt.Println(err.Error())
 }
